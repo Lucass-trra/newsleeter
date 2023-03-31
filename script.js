377c03392fe24fdd668802bea26a70dc158c37bd
Lucas Terra <lucasterracontato@gmail.com>
2023-03-31 13:32:42 -0300

    desenvolvendo funcionalidade modo escuro e claro para a página

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-var ancoras_menu = document.getElementsByClassName("ancoras_menu");
-var figura_menu = document.getElementById("figura_menu");
-var background_ancoras = document.getElementById("background_ancoras");
+// variáveis do menu com as âncoras
+    var ancoras_menu = document.getElementsByClassName("ancoras_menu");
+    var figura_menu = document.getElementById("figura_menu");
+    var background_ancoras = document.getElementById("background_ancoras");
+//variáveis com o modo escuro e claro
+    const html = document.querySelector("html")
+    var modo_claro_escuro = document.getElementById("modo_claro_escuro");
+    var figura_claro_escuro = document.getElementById("figura_claro_escuro");
+    
 
 // configrando o menu das âncoras
     background_ancoras.addEventListener("mouseover",function() {
@@ -18,3 +24,11 @@ var background_ancoras = document.getElementById("background_ancoras");
         }
     })
 
+//configurando o modo escuro e claro
+    modo_claro_escuro.addEventListener("click",function() {
+        html.classList.toggle("dark-mode");
+    })
+
+    
+        
+        
